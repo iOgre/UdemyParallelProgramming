e83fdd1127c1bb92f35546e9e65da9964c28f35c
IGOR-B50\ingwar <ingwahr@outlook.com>
2018-01-11 01:20:35 +0500

    waiting time to pass
    Signed-off-by: IGOR-B50\ingwar <ingwahr@outlook.com>

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -12,16 +12,29 @@ namespace Terminal
         {
             var cts = new CancellationTokenSource();
             var token = cts.Token;
-             
             var t = new Task(() =>
             {
-                Console.WriteLine("Press any key to disarm, you have 5 seconds");
-              bool cancelled =  token.WaitHandle.WaitOne(5000);
-               Console.WriteLine(cancelled ? "Disarmed" : "Boom");
+                Console.WriteLine("I take 5 seconds");
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("I am done in 5 seconds");
+                
             }, token);
-            Console.ReadKey();
-            cts.Cancel();
-            
+            t.Start();
+            Task t2 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("I take 3 seconds");
+                Thread.Sleep(3000);
+                Console.WriteLine("3 seconds wait done");
+            }, token);
+
+
+            Task.WaitAll( new [] {t, t2}, 4000);
+             Console.WriteLine($" Task t status is {t.Status}, task t2 status is {t2.Status}");
+            //Console.ReadKey();
             Console.WriteLine("Main program done");
             Console.ReadKey();
         }
