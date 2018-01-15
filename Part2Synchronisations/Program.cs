714445c1d0def24dd8ad903b835cb1af0639a8ac
IGOR-B50\ingwar <ingwahr@outlook.com>
2018-01-16 03:44:16 +0500

    interlock
    Signed-off-by: IGOR-B50\ingwar <ingwahr@outlook.com>

--- Part2Synchronisations/Program.cs
+++ Part2Synchronisations/Program.cs
@@ -1,33 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Part2Synchronisations
 {
     public class BankAccount
     {
-        private object padLock = new Object();
-        public int Balance { get; private set; }
+        private int _balance;
+
+        public int Balance
+        {
+            get { return _balance; }
+            private set { _balance = value; }
+        }
 
         public void Deposit(int amount)
         {
             //+=
             // op1: temp <- get_Balance() + amount
             // op2: set_Balance(temp)
-            lock (padLock)
-            {
-                Balance += amount;
-            }
+            Interlocked.Add(ref _balance, amount);
         }
 
 
         public void Widthdraw(int amount)
         {
-            lock (padLock)
-            {
-                Balance -= amount;
-            }
+            Interlocked.Add(ref _balance, -amount);
         }
     }
 
