da906510c435ae032f21d171caaff96667cb62df
IGOR-B50\ingwar <ingwahr@outlook.com>
2018-01-17 13:25:54 +0500

    lock recursion
    Signed-off-by: IGOR-B50\ingwar <ingwahr@outlook.com>

---
+++ LockRecursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace LockRecursion
+{
+    class Program
+    {
+        private static SpinLock sl = new SpinLock(true);
+        static void Main(string[] args)
+        {
+            LockRecursion(5);
+        }
+
+        public static void LockRecursion(int x)
+        {    
+            Console.WriteLine($"X = {x}");
+            bool lockTaken = false;
+            try
+            {
+                sl.Enter(ref lockTaken);
+
+            }
+            catch (LockRecursionException ex)
+            {
+                Console.WriteLine("Exception" + ex);
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Console.WriteLine($"Took a lock x = {x}");
+                    LockRecursion(x - 1);
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to take a lock x = {x}");
+                }
+
+            }
+        }
+    }
+    
+}
